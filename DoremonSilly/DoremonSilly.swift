59a5e37fee889c9da979ab32a46ec241f38ba799
devdcs <dev@diachiso.vn>
2021-04-23 15:02:50 +0700

    framework

---
+++ DoremonSilly/DoremonSilly.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  DoremonSilly
+//
+//  Created by iMac on 23/04/2021.
+//
+
+import Foundation
+
+public struct DoremonSilly{
+    public static func showName(){
+        print("Doremon Silly")
+    }
+}
